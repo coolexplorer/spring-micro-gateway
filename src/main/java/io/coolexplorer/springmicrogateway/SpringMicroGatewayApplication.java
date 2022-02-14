46ddd1b80046f04cd3e7e92c9804bde827afeb0e
seunkim <seunkim@ea.com>
2022-02-14 12:37:24 -0800

    [[gateway] Create Initial Project]

---
+++ src/main/java/io/coolexplorer/springmicrogateway/SpringMicroGatewayApplication.java
@@ -0,0 +1,13 @@
+package io.coolexplorer.springmicrogateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringMicroGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringMicroGatewayApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/coolexplorer/springmicrogateway/SpringMicroGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package io.coolexplorer.springmicrogateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringMicroGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
